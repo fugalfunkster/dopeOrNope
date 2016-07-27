1add71d64189af8d8d29a271245e0e3e1750d266
fugalfunkster <mtthwhsltn@gmail.com>
2016-07-26 20:47:23 -0700

    move from codepen to github repo

---
+++ index.js
@@ -0,0 +1,518 @@
+var data = [
+  {
+    "Time": "36:50",
+    "Place": 1,
+    "Seconds": 2210,
+    "Name": "Marco Pantani",
+    "Year": 1995,
+    "Nationality": "ITA",
+    "Doping": "Alleged drug use during 1995 due to high hematocrit levels",
+    "URL": "https://en.wikipedia.org/wiki/Marco_Pantani#Alleged_drug_use"
+  },
+  {
+    "Time": "36:55",
+    "Place": 2,
+    "Seconds": 2215,
+    "Name": "Marco Pantani",
+    "Year": 1997,
+    "Nationality": "ITA",
+    "Doping": "Alleged drug use during 1997 due to high hermatocrit levels",
+    "URL": "https://en.wikipedia.org/wiki/Marco_Pantani#Alleged_drug_use"
+  },
+  {
+    "Time": "37:15",
+    "Place": 3,
+    "Seconds": 2235,
+    "Name": "Marco Pantani",
+    "Year": 1994,
+    "Nationality": "ITA",
+    "Doping": "Alleged drug use during 1994 due to high hermatocrit levels",
+    "URL": "https://en.wikipedia.org/wiki/Marco_Pantani#Alleged_drug_use"
+  },
+  {
+    "Time": "37:36",
+    "Place": 4,
+    "Seconds": 2256,
+    "Name": "Lance Armstrong",
+    "Year": 2004,
+    "Nationality": "USA",
+    "Doping": "2004 Tour de France title stripped by UCI in 2012",
+    "URL": "https://en.wikipedia.org/wiki/History_of_Lance_Armstrong_doping_allegations"
+  },
+  {
+    "Time": "37:42",
+    "Place": 5,
+    "Seconds": 2262,
+    "Name": "Jan Ullrich",
+    "Year": 1997,
+    "Nationality": "GER",
+    "Doping": "Confessed later in his career to doping",
+    "URL": "https://en.wikipedia.org/wiki/Jan_Ullrich#Operaci.C3.B3n_Puerto_doping_case"
+  },
+  {
+    "Time": "38:05",
+    "Place": 6,
+    "Seconds": 2285,
+    "Name": "Lance Armstrong",
+    "Year": 2001,
+    "Nationality": "USA",
+    "Doping": "2001 Tour de France title stripped by UCI in 2012",
+    "URL": "https://en.wikipedia.org/wiki/History_of_Lance_Armstrong_doping_allegations"
+  },
+  {
+    "Time": "38:14",
+    "Place": 7,
+    "Seconds": 2294,
+    "Name": "Miguel Indurain",
+    "Year": 1995,
+    "Nationality": "ESP",
+    "Doping": "1994 Failed test for salbutemol, not a banned drug at that time",
+    "URL": "http://www.independent.co.uk/sport/drugs-in-sport-indurain-allowed-to-use-banned-drug-1379584.html"
+  },
+  {
+    "Time": "38:14",
+    "Place": 8,
+    "Seconds": 2294,
+    "Name": "Alex Zülle",
+    "Year": 1995,
+    "Nationality": "SUI",
+    "Doping": "Confessed in 1998 to taking EPO",
+    "URL": "https://en.wikipedia.org/wiki/Alex_Z%C3%BClle#Festina_affair"
+  },
+  {
+    "Time": "38:16",
+    "Place": 9,
+    "Seconds": 2296,
+    "Name": "Bjarne Riis",
+    "Year": 1995,
+    "Nationality": "DEN",
+    "Doping": "Alleged drug use during 1995 due to high hermatrocite levels",
+    "URL": "https://en.wikipedia.org/wiki/Bjarne_Riis#Doping_allegations"
+  },
+  {
+    "Time": "38:22",
+    "Place": 10,
+    "Seconds": 2302,
+    "Name": "Richard Virenque",
+    "Year": 1997,
+    "Nationality": "FRA",
+    "Doping": "In 2000 confessed to doping during his career",
+    "URL": "https://en.wikipedia.org/wiki/Richard_Virenque#Festina_affair"
+  },
+  {
+    "Time": "38:36",
+    "Place": 11,
+    "Seconds": 2316,
+    "Name": "Floyd Landis",
+    "Year": 2006,
+    "Nationality": "USA",
+    "Doping": "Stripped of 2006 Tour de France title",
+    "URL": "https://en.wikipedia.org/wiki/Floyd_Landis_doping_case"
+  },
+  {
+    "Time": "38:36",
+    "Place": 12,
+    "Seconds": 2316,
+    "Name": "Andreas Klöden",
+    "Year": 2006,
+    "Nationality": "GER",
+    "Doping": "Alleged use of illegal blood transfusions in 2006",
+    "URL": "https://en.wikipedia.org/wiki/Andreas_Kl%C3%B6den#2009_Doping_allegations"
+  },
+  {
+    "Time": "38:40",
+    "Place": 13,
+    "Seconds": 2320,
+    "Name": "Jan Ullrich",
+    "Year": 2004,
+    "Nationality": "GER",
+    "Doping": "Confessed later in his career to doping",
+    "URL": "https://en.wikipedia.org/wiki/Jan_Ullrich#Operaci.C3.B3n_Puerto_doping_case"
+  },
+  {
+    "Time": "38:44",
+    "Place": 14,
+    "Seconds": 2324,
+    "Name": "Laurent Madouas",
+    "Year": 1995,
+    "Nationality": "FRA",
+    "Doping": "Tested positive for Salbutemol in 1994, suspended for 1 month",
+    "URL": "http://www.dopeology.org/incidents/Madouas-positive/"
+  },
+  {
+    "Time": "38:55",
+    "Place": 15,
+    "Seconds": 2335,
+    "Name": "Richard Virenque",
+    "Year": 1994,
+    "Nationality": "FRA",
+    "Doping": "In 2000 confessed to doping during his career",
+    "URL": "https://en.wikipedia.org/wiki/Richard_Virenque#Festina_affair"
+  },
+  {
+    "Time": "39:01",
+    "Place": 16,
+    "Seconds": 2341,
+    "Name": "Carlos Sastre",
+    "Year": 2006,
+    "Nationality": "ESP",
+    "Doping": "",
+    "URL": ""
+  },
+  {
+    "Time": "39:09",
+    "Place": 17,
+    "Seconds": 2349,
+    "Name": "Iban Mayo",
+    "Year": 2003,
+    "Nationality": "ESP",
+    "Doping": "Failed doping test in 2007 Tour de France",
+    "URL": "https://en.wikipedia.org/wiki/Iban_Mayo"
+  },
+  {
+    "Time": "39:12",
+    "Place": 18,
+    "Seconds": 2352,
+    "Name": "Andreas Klöden",
+    "Year": 2004,
+    "Nationality": "GER",
+    "Doping": "Alleged doping during 2006 Tour de France",
+    "URL": "https://en.wikipedia.org/wiki/Operaci%C3%B3n_Puerto_doping_case"
+  },
+  {
+    "Time": "39:14",
+    "Place": 19,
+    "Seconds": 2354,
+    "Name": "Jose Azevedo",
+    "Year": 2004,
+    "Nationality": "POR",
+    "Doping": "Implicated in the Operación Puerto doping case",
+    "URL": "https://en.wikipedia.org/wiki/Operaci%C3%B3n_Puerto_doping_case"
+  },
+  {
+    "Time": "39:15",
+    "Place": 20,
+    "Seconds": 2355,
+    "Name": "Levi Leipheimer",
+    "Year": 2006,
+    "Nationality": "USA",
+    "Doping": "Testified in 2012 to doping during his time with US Postal Service ",
+    "URL": "http://www.wsj.com/articles/SB10000872396390444799904578048352672452328"
+  },
+  {
+    "Time": "39:22",
+    "Place": 21,
+    "Seconds": 2362,
+    "Name": "Francesco Casagrande",
+    "Year": 1997,
+    "Nationality": "ITA",
+    "Doping": "Positive testosterone test in 1998",
+    "URL": "http://autobus.cyclingnews.com/results/1998/sep98/sep2.shtml"
+  },
+  {
+    "Time": "39:23",
+    "Place": 22,
+    "Seconds": 2363,
+    "Name": "Nairo Quintana",
+    "Year": 2015,
+    "Nationality": "COL",
+    "Doping": "",
+    "URL": ""
+  },
+  {
+    "Time": "39:23",
+    "Place": 23,
+    "Seconds": 2363,
+    "Name": "Bjarne Riis",
+    "Year": 1997,
+    "Nationality": "DEN",
+    "Doping": "Alleged drug use during 1995 due to high hermatrocite levels",
+    "URL": "https://en.wikipedia.org/wiki/Bjarne_Riis#Doping_allegations"
+  },
+  {
+    "Time": "39:30",
+    "Place": 24,
+    "Seconds": 2370,
+    "Name": "Miguel Indurain",
+    "Year": 1994,
+    "Nationality": "ESP",
+    "Doping": "1994 Failed test for salbutemol, not a banned drug at that time",
+    "URL": "http://www.independent.co.uk/sport/drugs-in-sport-indurain-allowed-to-use-banned-drug-1379584.html"
+  },
+  {
+    "Time": "39:30",
+    "Place": 25,
+    "Seconds": 2370,
+    "Name": "Luc Leblanc",
+    "Year": 1994,
+    "Nationality": "FRA",
+    "Doping": "Admitted to using epo and amphetimines throughout 1994 ",
+    "URL": "http://www.dopeology.org/people/Luc_Leblanc/"
+  },
+  {
+    "Time": "39:32",
+    "Place": 26,
+    "Seconds": 2372,
+    "Name": "Carlos Sastre",
+    "Year": 2008,
+    "Nationality": "ESP",
+    "Doping": "",
+    "URL": ""
+  },
+  {
+    "Time": "39:37",
+    "Place": 27,
+    "Seconds": 2377,
+    "Name": "Vladimir Poulnikov",
+    "Year": 1994,
+    "Nationality": "UKR",
+    "Doping": "",
+    "URL": ""
+  },
+  {
+    "Time": "39:40",
+    "Place": 28,
+    "Seconds": 2380,
+    "Name": "Giuseppe Guerini",
+    "Year": 2004,
+    "Nationality": "ITA",
+    "Doping": "",
+    "URL": ""
+  },
+  {
+    "Time": "39:41",
+    "Place": 29,
+    "Seconds": 2381,
+    "Name": "Santos Gonzalez",
+    "Year": 2004,
+    "Nationality": "ESP",
+    "Doping": "High Hematocrit during 2005 season, removed by team management",
+    "URL": "http://www.cyclingnews.com/news/santos-gonzalez-sacked-by-phonak/"
+  },
+  {
+    "Time": "39:41",
+    "Place": 30,
+    "Seconds": 2381,
+    "Name": "Vladimir Karpets",
+    "Year": 2004,
+    "Nationality": "RUS",
+    "Doping": "Made payments to Ferrari, but no charges filed",
+    "URL": "http://www.dopeology.org/incidents/Ferrari-investigation/"
+  },
+  {
+    "Time": "39:45",
+    "Place": 31,
+    "Seconds": 2385,
+    "Name": "Fernando Escartin",
+    "Year": 1995,
+    "Nationality": "ESP",
+    "Doping": "Implicated in Giardini Margherita Raid in 1998 as a 'victim' ",
+    "URL": "http://www.dopeology.org/incidents/Giardini-Margherita-raid-%5bBologna%5d/"
+  },
+  {
+    "Time": "39:47",
+    "Place": 32,
+    "Seconds": 2387,
+    "Name": "Denis Menchov",
+    "Year": 2006,
+    "Nationality": "RUS",
+    "Doping": "",
+    "URL": ""
+  },
+  {
+    "Time": "39:47",
+    "Place": 33,
+    "Seconds": 2387,
+    "Name": "Michael Rasmussen",
+    "Year": 2006,
+    "Nationality": "DEN",
+    "Doping": "Admitted to doping with multiple substances 1998-2010",
+    "URL": "http://www.dopeology.org/people/Michael_Rasmussen/"
+  },
+  {
+    "Time": "39:47",
+    "Place": 34,
+    "Seconds": 2387,
+    "Name": "Pietro Caucchioli",
+    "Year": 2006,
+    "Nationality": "ITA",
+    "Doping": "Associated with Mantova investigation, charges dropped",
+    "URL": "http://www.cyclingnews.com/news/italian-judge-set-to-decide-if-32-named-in-mantova-doping-investigation-should-go-on-trial/"
+  },
+  {
+    "Time": "39:50",
+    "Place": 35,
+    "Seconds": 2390,
+    "Name": "Nairo Quintana",
+    "Year": 2013,
+    "Nationality": "COL",
+    "Doping": "",
+    "URL": ""
+  }
+];
+
+// dimensions
+var height = 600, width = 900,
+    margin = {top: 10, right: 80, bottom: 10, left: 40};
+
+// scale
+var xScale = d3.scale.linear()
+  .range([0, width]);
+var yScale = d3.scale.linear()
+  .range([height, 0]);
+
+// domain
+xScale.domain([200, -30]);
+yScale.domain([39, -4]);
+
+// axes
+var xAxis = d3.svg.axis()
+  .scale(xScale)
+  .orient("bottom")
+  .tickValues([180, 150, 120, 90, 60, 30, 0]);
+var yAxis = d3.svg.axis()
+  .scale(yScale)
+  .orient("left")
+  .tickValues([1, 5, 10, 15, 20, 25, 30, 35]);
+
+// color dots accordingly
+function dotColor(d){
+  if(d.Doping){ return "red"; }
+  else { return "#666666"; }
+}
+
+// anchor our viz
+var svg = d3.select("body").append("svg")
+  .attr("height", height + margin.left + margin.right)
+  .attr("width", width + margin.top + margin.bottom);
+
+// create inset chart
+var chart = svg.append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// X axis labels
+chart.append("g")
+	    .attr("class", "x axis")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(xAxis)
+	  .append("text")
+	    .attr("x", width*2/3)
+	    .attr("y", 40)
+	    .style("text-anchor", "end")
+	    .text("Seconds Behind Leader");
+
+// Y axis labels
+chart.append("g")
+	    .attr("class", "y axis")
+	    .call(yAxis)
+	  .append("text")
+	    .attr("transform", "rotate(-90)")
+	    .attr("y", -40)
+	    .attr("dy", ".9em")
+	    .style("text-anchor", "end")
+	    .text("Ranking");
+
+// Titles
+chart.append("text")
+        .attr("x", (width / 2))             
+        .attr("y", 10)
+        .attr("text-anchor", "middle")  
+        .style("text-decoration", "underline")  
+        .text("Doping in Professional Bicycle Racing")
+        .style("font-size", "24px") 
+        .style("text-decoration", "underline");
+chart.append("text")
+        .attr("x", (width / 2))             
+        .attr("y", 35)
+        .attr("text-anchor", "middle")  
+        .style("text-decoration", "underline")  
+        .text("35 Fastest times up Alpe d'Huez")
+        .style("font-size", "20px") 
+        .style("text-decoration", "underline");
+chart.append("text")
+        .attr("x", (width / 2))             
+        .attr("y", 55)
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px") 
+        .style("text-decoration", "underline")  
+        .text("Normalized to 13.8km distance")
+        .style("text-decoration", "underline");
+
+// build a tool tip
+var tooltip = d3.select("body").append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
+// plot data into circles
+var dots = chart.selectAll("circle")
+  .data(data)
+  .enter()
+  .append("circle")
+  .attr({
+  cx: function(d){ return xScale(d.Seconds - data[0].Seconds); },
+  cy: function(d){ return yScale(d.Place); },
+  r : 5,
+  "fill": function(d){ return dotColor(d); },
+})
+// tooltip event handlers
+.on("mouseover", function(d){
+  d3.select(this).style("fill", "blue");
+  tooltip.style("opacity", .95);
+  tooltip.html("<div><p>" + d.Name + ": " + d.Nationality + "</p><p>Year: " + d.Year + "  Time: " + d.Time +"</p><p>" + d.Doping + "</p></div>")
+    .style("left", (d3.event.pageX + 18) + "px")
+    .style("top", (d3.event.pageY - 28) + "px");
+  })
+.on("mouseout", function(d){
+    d3.select(this).style("fill", function(d){ return dotColor(d); });
+    tooltip.style("opacity", 0);
+  });
+
+
+// label each circle
+var labels = dots.select("text")
+  .data(data)
+  .enter()
+  .append("text")
+.text(function (d) { return d.Name; })
+.attr({
+  x: function(d){ return 8 + xScale(d.Seconds - data[0].Seconds); },
+  y: function(d){ return 5 + yScale(d.Place); },
+  "font-size": "12px",
+  "font-family": "sans-serif",
+  "font-fill": "#666666",
+  "text-anchor" : "start"
+});
+
+//Create a Legend
+var legend = svg.selectAll(".legend").data([1,2]).enter().append("g")
+    .attr({
+      x: 500,
+      y: 200,
+      "transform": "translate(150, 150)",
+      class: "legend",
+      "font-size": "12px",
+      "font-family": "sans-serif",
+      "font-fill": "#666666",
+    });
+
+//create legend key for Nope
+legend.append("circle")
+    .attr({ cy: 20, r : 5,
+      "fill": "red" });
+legend.append("text")
+    .attr("x", 40)
+    .attr("y", 5)
+    .style("text-anchor", "end")
+    .text("Nope");
+
+//create legend key for Dope
+legend.append("circle")
+    .attr({ x: 50, y: 20, r : 5,
+      "fill": "#666666"});
+legend.append("text")
+    .attr("x", 40)
+    .attr("y", 25)
+    .style("text-anchor", "end")
+    .text("Dope");
+
+
